02e992f068e204fea077fcd986d12c00efa38a62
sunu-nandakumar <appu1998.nandakumar@gmail.com>
2021-06-27 18:40:53 +0530

    code refactored and adding the remaining players

--- DeckOFCards/src/cards/DeckOfCards.java
+++ DeckOFCards/src/cards/DeckOfCards.java
@@ -7,38 +7,51 @@ public class DeckOfCards {
 	public static void main(String[] args) {
 
 		String[][] cardCombo = new String[4][13];
-		String[][] playerCards = new String[4][9];
+		String[] cardsDistrubed = new String[36];
+		String[][] player = new String[4][9];
 		Set<String> cards = new HashSet<String>();
 
-		
-		//
 		String[] suits = { "Clubs", "Hearts", "Diamonds", "Spade" };
 		String[] rank = { "Ace", "Kings", "Queen", "Jack", "2", "3", "4", "5", "6", "7", "8", "9", "10" };
 
+			// creating all the possible combinations
 		for (int i = 0; i < 4; i++) {
 			for (int j = 0; j < 13; j++) {
 				cardCombo[i][j] = suits[i] + rank[j];
-				
+
 			}
 		}
-
-		String[] player = new String[9];
-	
-	
-		while (cards.size() < 9) {
+		// to avoid duplicate cards 
+		while (cards.size() < 36) {
 			int index = cards.size();
 			int randomSuits = (int) Math.floor(Math.random() * 10) % 4;
 			int randomRanks = (int) Math.floor(Math.random() * 100) % 13;
-			
-			
-		
-			player[index] = cardCombo[randomSuits][randomRanks];
-			cards.add(player[index]);
-			
+
+			cardsDistrubed[index] = cardCombo[randomSuits][randomRanks];
+			cards.add(cardsDistrubed[index]);
 
 		}
-		System.out.println(cards);
-		System.out.println(cards.size());
+		// distributing the cards to players
+		int counter = 0;
+		while (counter < 36) {
+
+			for (int i = 0; i < 4; i++) {
+				for (int j = 0; j < 9; j++) {
+					player[i][j] = (String) cards.toArray()[counter];
+					counter++;
+
+				}
+			}
+		}
+		// printing the 2-D array 
+		for (int i = 0; i < 4; i++) {
+			System.out.println("\nPlayer " + (i + 1) + " cards\n");
+			for (int j = 0; j < 9; j++) {
+
+				System.out.println(player[i][j]);
+			}
+		}
+
 	}
 
 }
